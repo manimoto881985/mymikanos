3d7915ef7a28bac6e95a72620c06df379bda0614
manimoto881985 <manimoto88@gmail.com>
2021-09-14 11:30:59 +0900

    day05

--- kernel/graphics.cpp
+++ kernel/graphics.cpp
@@ -20,3 +20,24 @@ void BGRResv8BitColorPixelWriter::Write(int x, int y, const PixelColor& c) {
   p[1] = c.g;
   p[2] = c.r;
 }
+
+void DrawRectangle(PixelWriter& writer, const Vector2D<int>& pos,
+                   const Vector2D<int>& size, const PixelColor& c) {
+  for (int dx = 0; dx < size.x; ++dx) {
+    writer.Write(pos.x + dx, pos.y, c);
+    writer.Write(pos.x + dx, pos.y + size.y - 1, c);
+  }
+  for (int dy = 0; dy < size.y; ++dy) {
+    writer.Write(pos.x, pos.y + dy, c);
+    writer.Write(pos.x + size.x, pos.y + dy - 1, c);
+  }
+}
+
+void FillRectangle(PixelWriter& writer, const Vector2D<int>& pos,
+                   const Vector2D<int>& size, const PixelColor& c) {
+  for (int dy = 0; dy < size.y; ++dy) {
+    for (int dx = 0; dx < size.x; ++dx) {
+      writer.Write(pos.x + dx, pos.y + dy, c);
+    }
+  }
+}
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,9 +1,11 @@
 #include <cstdint>
 #include <cstddef>
+#include <cstdio>
 
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "font.hpp"
+#include "console.hpp"
 
 void* operator new(size_t size, void* buf) {
   return buf;
@@ -12,8 +14,56 @@ void* operator new(size_t size, void* buf) {
 void operator delete(void* obj) noexcept {
 }
 
+const PixelColor kDesktopBGColor{45, 118, 237};
+const PixelColor kDesktopFGColor{255, 255, 255};
+
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+  "@              ",
+  "@@             ",
+  "@.@            ",
+  "@..@           ",
+  "@...@          ",
+  "@....@         ",
+  "@.....@        ",
+  "@......@       ",
+  "@.......@      ",
+  "@........@     ",
+  "@.........@    ",
+  "@..........@   ",
+  "@...........@  ",
+  "@............@ ",
+  "@......@@@@@@@@",
+  "@......@       ",
+  "@....@@.@      ",
+  "@...@ @.@      ",
+  "@..@   @.@     ",
+  "@.@    @.@     ",
+  "@@      @.@    ",
+  "@       @.@    ",
+  "         @.@   ",
+  "         @@@   ",
+};
+
 char pixel_writer_buf[sizeof(RGBResv8BitColorPixelWriter)];
 PixelWriter* pixel_writer;
+
+char console_buf[sizeof(Console)];
+Console* console;
+
+int printk(const char* format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
@@ -27,20 +77,40 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       break;
   }
 
-  for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
-    for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-      pixel_writer->Write(x, y, {255, 255, 255});
-    }
-  }
-  for (int x = 0; x < 200; ++x) {
-    for (int y = 0; y < 100; ++y) {
-      pixel_writer->Write(x, y, {0, 255, 0});
-    }
-  }
+  const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+  const int kFrameHeight = frame_buffer_config.vertical_resolution;
 
-  WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
-  WriteAscii(*pixel_writer, 58, 50, 'B', {0, 0, 0});
-  WriteAscii(*pixel_writer, 66, 50, 'C', {0, 0, 0});
+  FillRectangle(*pixel_writer,
+                {0, 0},
+                {kFrameWidth, kFrameHeight - 50},
+                kDesktopBGColor);
+  FillRectangle(*pixel_writer,
+                {0, kFrameHeight - 50},
+                {kFrameWidth, 50},
+                {1, 8, 17});
+  FillRectangle(*pixel_writer,
+                {0, kFrameHeight - 50},
+                {kFrameWidth / 5, 50},
+                {80, 80, 80});
+  DrawRectangle(*pixel_writer,
+                {10, kFrameHeight - 40},
+                {30, 30},
+                {160, 160, 160});
+
+  console = new(console_buf) Console{
+    *pixel_writer, kDesktopFGColor, kDesktopBGColor
+  };
+  printk("Welcome to MikanOS!\n");
+
+  for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
+    for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
+      if (mouse_cursor_shape[dy][dx] == '@') {
+        pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
+      } else if (mouse_cursor_shape[dy][dx] == '.') {
+        pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+      }
+    }
+  }
 
   while (1) __asm__("hlt");
 }
