e5790e9aa54e1162a97509f98462e6f9eac02c3c
manimoto881985 <manimoto88@gmail.com>
2021-10-25 12:47:32 +0900

    day13b

--- kernel/timer.cpp
+++ kernel/timer.cpp
@@ -2,6 +2,7 @@
 
 #include "acpi.hpp"
 #include "interrupt.hpp"
+#include "task.hpp"
 
 namespace {
   const uint32_t kCountMax = 0xffffffffu;
@@ -50,12 +51,21 @@ void StopLAPICTimer() {
   initial_count = 0;
 }
 
-void TimerManager::Tick() {
+bool TimerManager::Tick() {
   ++tick_;
+
+  bool task_timer_timeout = false;
   while (true) {
     const auto& t = timers_.top();
     if (t.Timeout() > tick_) {
       break;
+    }
+
+    if (t.Value() == kTaskTimerValue) {
+      task_timer_timeout = true;
+      timers_.pop();
+      timers_.push(Timer{tick_ + kTaskTimerPeriod, kTaskTimerValue});
+      continue;
     }
 
     Message m{Message::kTimerTimeout};
@@ -65,11 +75,18 @@ void TimerManager::Tick() {
 
     timers_.pop();
   }
+
+  return task_timer_timeout;
 }
 
 TimerManager* timer_manager;
 unsigned long lapic_timer_frep;
 
 void LAPICTimerOnInterrupt() {
-  timer_manager->Tick();
+  const bool task_timer_timeout = timer_manager->Tick();
+  NotifyEndOfInterrupt();
+
+  if (task_timer_timeout) {
+    SwitchTask();
+  }
 }
